bfaa6a76590dbf68ef068a06c958d199b2cab219
MerlyneIradukunda <merlyneirad05@gmail.com>
2022-01-20 21:10:36 +0200

    fix: depreceated syntax

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const createError = require("http-errors");
-
+const mongoose= require("mongoose");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+
 mongoose
-  .connect("mongodb://localhost/tddDB", {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  })
+  .connect("mongodb://localhost/tddDB", {})
   .then(() =>
     console.log(`Connected to MongoDB at mongodb://localhost/tddDB...`)
   )
@@ -20,22 +15,12 @@ mongoose
     process.exit();
   });
 
-const usersRouter = require("./routes/user.route");
-
-app.use("/api/users", usersRouter);
-
-app.use((req, res, next) => {
-  next(createError(404));
+app.get('/api/home/', (req, res) => {
+  res.send({ message: 'Hello WWW!' });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-
-  // render the error page
-  res.status(err.status || 500);
-  res.send(err);
+app.listen(3008, () => {
+  console.log('Application listening on port 3008!');
 });
 
 module.exports = app;
