12a116c5f86f5e0e1a0d6c7d442369109d5303dd
MerlyneIradukunda <merlyneirad05@gmail.com>
2022-01-25 21:57:38 +0200

    Feat:added user get all endpoint

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const mongoose= require("mongoose");
+require('dotenv').config()
+
+
+const usersRouter = require("./src/routes/user.route");
+
+var uri=`mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.etiat.mongodb.net/TestDB?retryWrites=true&w=majority`;
+
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
 mongoose
-  .connect("mongodb://localhost/tddDB", {})
+  .connect(uri)
   .then(() =>
-    console.log(`Connected to MongoDB at mongodb://localhost/tddDB...`)
+    console.log(`Connected to MongoDB successfully...`)
   )
   .catch(err => {
     console.log("Failed to connect to MongoDB...", err);
@@ -16,11 +23,14 @@ mongoose
   });
 
 app.get('/api/home/', (req, res) => {
-  res.send({ message: 'Hello WWW!' });
+  res.send( 'Hello World!' );
 });
 
-app.listen(3008, () => {
-  console.log('Application listening on port 3008!');
+app.use("/api/users", usersRouter);
+
+$PORT=2012;
+app.listen($PORT, () => {
+  console.log('Application listening on port '+$PORT);
 });
 
 module.exports = app;
